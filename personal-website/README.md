d25be5692391f3e61c49d3da14f36f6f5349aaf0
Dang Mai <contact@dangmai.net>
2013-05-13 15:39:27 -0400

    Add skeleton for personal website cookbook

---
+++ personal-website/README.md
@@ -0,0 +1,13 @@
+# personal-website cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ personal-website/metadata.rb
@@ -0,0 +1,8 @@
+name             "personal-website"
+maintainer       "YOUR_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures personal-website"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
