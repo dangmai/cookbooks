83fdee89211559c82f9227ccaa9830249b792f06
Dang Mai <contact@dangmai.net>
2013-05-13 15:52:01 -0400

    Modify generated metadata file and add dependency

--- personal-website/metadata.rb
+++ personal-website/metadata.rb
@@ -1,8 +1,10 @@
 name             "personal-website"
-maintainer       "YOUR_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Dang Mai"
+maintainer_email "contact@dangmai.net"
 license          "All rights reserved"
-description      "Installs/Configures personal-website"
+description      "Installs/Configures my personal website"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+depends "apache2"
+depends "npm"
