cf3ba772deeaabe1c750f43f9d386e0343e36f30
Dang Mai <contact@dangmai.net>
2013-05-17 21:44:45 -0400

    Minor changes to metadata

--- personal-website/metadata.rb
+++ personal-website/metadata.rb
@@ -1,7 +1,7 @@
 name             "personal-website"
 maintainer       "Dang Mai"
 maintainer_email "contact@dangmai.net"
-license          "All rights reserved"
+license          "MIT"
 description      "Installs/Configures my personal website"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
--- splitwizard/metadata.rb
+++ splitwizard/metadata.rb
@@ -1,8 +1,8 @@
 name             "splitwizard"
 maintainer       "Dang Mai"
 maintainer_email "contact@dangmai.net"
-license          "All rights reserved"
-description      "Installs/Configures my SplitWizard"
+license          "MIT"
+description      "Installs/Configures SplitWizard"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
